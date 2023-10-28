a49b4585cc9af402646339870aff35b8310109ef
Md Anas Sabah <mdanassabah@gmail.com>
2023-10-29 02:31:29 +0530

    fixed minor bug ~ css

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,16 +3,11 @@ import Button from "../../components/Button";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col bg-neutral-1000 h-screen w-screen  md:justify-center md:flex-row  md:gap-100">
-      <section className="flex justify-center items-center py-3 px-4">
-        <img
-          src={logo}
-          alt="logo"
-          className="h-mobile-view-logo w-mobile-view-logo md:h-desktop-view-logo md:w-desktop-view-logo"
-        />
-      </section>
-      <section className="flex flex-col px-7 gap-10 md:w-[490px]  mt-auto mb-auto">
+      <Logo />
+      <section className="flex flex-col px-7 gap-10  mt-auto mb-auto">
         <div className="flex flex-col gap-3 self-stretch text-neutral-50">
           <div className="font-px-regular font-extrabold text-31px md:font-bold md:text-5xl">
             Happening Now
@@ -21,9 +16,7 @@ function Login() {
             Join today.
           </div>
         </div>
-
         <Button variant="default" text="Create account" />
-
         <div className="flex items-center gap-2 w-full">
           <div className="w-full h-px bg-neutral-700" />
           <span className="font-chirp text-center text-base font-normal text-neutral-50">
@@ -35,14 +28,24 @@ function Login() {
           <p className="font-px-regular text-xl font-medium text-neutral-50">
             Already have an account?
           </p>
-          <Button
-            variant="outline"
-            text="Sign in"
-            onClick={useNavigate("/tweet")}
-          />
+          <div onClick={() => navigate("/tweet")}>
+            <Button variant="outline" text="Sign in" />
+          </div>
         </div>
       </section>
     </div>
+  );
+}
+
+function Logo() {
+  return (
+    <header className="flex justify-center items-center py-3 px-4">
+      <img
+        src={logo}
+        alt="logo"
+        className="h-mobile-view-logo w-mobile-view-logo md:h-desktop-view-logo md:w-desktop-view-logo"
+      />
+    </header>
   );
 }
 
