bda28239be77ca2f32624152d6f5b22232612238
Md Anas Sabah <mdanassabah@gmail.com>
2023-11-15 23:28:19 +0530

    handle authentication

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import logo from "../../assets/logo.png";
 import Button from "../../components/Button";
 import { useNavigate } from "react-router-dom";
 
+const BASE_URL = "https://one00x-react-backend.onrender.com";
+
 function Signup() {
+  const [showError, setShowError] = useState(false);
   const navigate = useNavigate();
+
+  const handleSignIn = async (email, password) => {
+    try {
+      const response = await fetch(`${BASE_URL}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.status === 200) {
+        navigate("/homefeed");
+      }
+      if (response.status === 400 || response.status === 401) {
+        const errorText = await response.text();
+        console.error(`Error ${response.status}: ${errorText}`);
+        setShowError(true);
+      }
+    } catch (e) {
+      console.log("Error", e);
+    }
+  };
+
   return (
     <div className="flex flex-col bg-neutral-1000 h-screen w-screen  md:justify-center md:flex-row  md:gap-100">
       <Logo />
@@ -30,9 +58,16 @@ function Signup() {
           <p className="font-px-regular text-xl font-medium text-neutral-50">
             Already have an account?
           </p>
-          <div onClick={() => navigate("/tweet")}>
+          <div
+            onClick={() => handleSignIn("batman@example.com", "gothamrocks")}
+          >
             <Button variant="outline" text="Sign in" />
           </div>
+          {showError && (
+            <h1 className="text-red-900 text-center font-bold font-px-regular">
+              User not found
+            </h1>
+          )}
         </div>
       </section>
     </div>
