4b343b1ec68ad33f04965f3a76af0db0e7112f05
Md Anas Sabah <mdanassabah@gmail.com>
2023-11-30 19:34:40 +0530

    adding signin modal

---
+++ src/components/SignInModal.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const SignInModal = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: Implement sign in logic here
+  };
+
+  return (
+    <div className="signin-modal">
+      <h2>Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignInModal;
