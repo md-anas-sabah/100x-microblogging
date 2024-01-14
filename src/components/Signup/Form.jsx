f1c6fe02e8309de8197e8d48cd6c6e31f756cccd
Md Anas Sabah <mdanassabah@gmail.com>
2024-01-15 00:09:48 +0530

    refactored the code

--- src/components/Signup/Form.jsx
+++ src/components/Signup/Form.jsx
@@ -4,8 +4,9 @@ import ChevronDown from "../../assets/ChevronDown.png";
 import Button from "../../components/Button";
 import InputFieldset from "./InputFieldset";
 import { useFormContext } from "../../context/FormContext";
+import { dates, months, years } from "../../utils/utils";
 
-const BASE_URL = "http://localhost:4000";
+// const BASE_URL = "http://localhost:4000";
 
 function Form({ onSubmit }) {
   const {
@@ -32,18 +33,18 @@ function Form({ onSubmit }) {
       dateOfBirth: dateOfBirth,
     };
 
-    try {
-      const response = await fetch(`${BASE_URL}/signup`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-      console.log(response);
-    } catch (err) {
-      console.log();
-    }
+    // try {
+    //   const response = await fetch(`${BASE_URL}/signup`, {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(formData),
+    //   });
+    //   console.log(response);
+    // } catch (err) {
+    //   console.log();
+    // }
 
     onSubmit(formData);
   };
@@ -95,18 +96,12 @@ function Form({ onSubmit }) {
               <option value="Month" hidden className="text-neutral-500">
                 Month
               </option>
-              <option value="january">January</option>
-              <option value="february">February</option>
-              <option value="march">March</option>
-              <option value="april">April</option>
-              <option value="may">May</option>
-              <option value="june">June</option>
-              <option value="july">July</option>
-              <option value="august">August</option>
-              <option value="september">September</option>
-              <option value="october">October</option>
-              <option value="november">November</option>
-              <option value="december">December</option>
+
+              {months.map((month) => (
+                <option value={month.toLowerCase()} key={month.toLowerCase()}>
+                  {month}
+                </option>
+              ))}
             </select>
             <img src={ChevronDown} alt="ChevronDown" className="h-6 w-6" />
           </fieldset>
@@ -127,37 +122,11 @@ function Form({ onSubmit }) {
               <option value="Day" hidden className="text-neutral-500">
                 Day
               </option>
-              <option value={1}>1</option>
-              <option value={2}>2</option>
-              <option value={3}>3</option>
-              <option value={4}>4</option>
-              <option value={5}>5</option>
-              <option value={6}>6</option>
-              <option value={7}>7</option>
-              <option value={8}>8</option>
-              <option value={9}>9</option>
-              <option value={10}>10</option>
-              <option value={11}>11</option>
-              <option value={12}>12</option>
-              <option value={13}>13</option>
-              <option value={14}>14</option>
-              <option value={15}>15</option>
-              <option value={16}>16</option>
-              <option value={17}>17</option>
-              <option value={18}>18</option>
-              <option value={19}>19</option>
-              <option value={20}>20</option>
-              <option value={21}>21</option>
-              <option value={22}>22</option>
-              <option value={23}>23</option>
-              <option value={24}>24</option>
-              <option value={25}>25</option>
-              <option value={26}>26</option>
-              <option value={27}>27</option>
-              <option value={28}>28</option>
-              <option value={29}>29</option>
-              <option value={30}>30</option>
-              <option value={31}>31</option>
+              {dates.map((date) => (
+                <option value={date} key={date}>
+                  {date}
+                </option>
+              ))}
             </select>
             <img src={ChevronDown} alt="ChevronDown" className="h-6 w-6" />
           </fieldset>
@@ -178,31 +147,11 @@ function Form({ onSubmit }) {
               <option value="Year" hidden className="text-neutral-500">
                 Year
               </option>
-              <option value={1999}>1999</option>
-              <option value={2000}>2000</option>
-              <option value={2001}>2001</option>
-              <option value={2002}>2002</option>
-              <option value={2003}>2003</option>
-              <option value={2004}>2004</option>
-              <option value={2005}>2005</option>
-              <option value={2006}>2006</option>
-              <option value={2007}>2007</option>
-              <option value={2008}>2008</option>
-              <option value={2009}>2009</option>
-              <option value={2010}>2010</option>
-              <option value={2011}>2011</option>
-              <option value={2012}>2012</option>
-              <option value={2013}>2013</option>
-              <option value={2014}>2014</option>
-              <option value={2015}>2015</option>
-              <option value={2016}>2016</option>
-              <option value={2017}>2017</option>
-              <option value={2018}>2018</option>
-              <option value={2019}>2019</option>
-              <option value={2020}>2020</option>
-              <option value={2021}>2021</option>
-              <option value={2022}>2022</option>
-              <option value={2023}>2023</option>
+              {years.map((year) => (
+                <option value={year} key={year}>
+                  {year}
+                </option>
+              ))}
             </select>
             <img src={ChevronDown} alt="ChevronDown" className="h-6 w-6" />
           </fieldset>
